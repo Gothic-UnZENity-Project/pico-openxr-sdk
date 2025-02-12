b246f6a53e4735ae5454ed4e33634daff8c6fbbb
Tyroonis <info@tyroonis.de>
2025-02-12 21:17:30 +0100

    update to pico openxr 1.3.3

--- Editor/PICOModifyAndroidManifest.cs
+++ Editor/PICOModifyAndroidManifest.cs
@@ -2,8 +2,11 @@
 using System.IO;
 using System.Text;
 using System.Xml;
+using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEditor.XR.OpenXR.Features;
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.OpenXR.Features;
 
 
 namespace Unity.XR.OpenXR.Features.PICOSupport
@@ -158,9 +161,20 @@ namespace Unity.XR.OpenXR.Features.PICOSupport
             internal void AddPICOMetaData(string path)
             {
                 CreateOrUpdateAndroidMetaData("pvr.app.type", "vr");
-                CreateOrUpdateAndroidMetaData("pvr.sdk.version", PICOFeature.SDKVersion);
+                CreateOrUpdateAndroidMetaData("pvr.sdk.version", "Unity OpenXR "+PICOFeature.SDKVersion);
                 CreateOrUpdateAndroidMetaData("pxr.sdk.version_code", "5800");
 
+                if (PICOProjectSetting.GetProjectConfig().isHandTracking)
+                {
+                    CreateOrUpdateAndroidPermissionData("com.picovr.permission.HAND_TRACKING");
+                    CreateOrUpdateAndroidMetaData("handtracking", "1");
+                }
+                else
+                {
+                    DeleteAndroidPermissionData("com.picovr.permission.HAND_TRACKING");
+                    DeleteAndroidMetaData("handtracking");
+                }
+                
                 if (PICOProjectSetting.GetProjectConfig().isEyeTracking)
                 {
                     CreateOrUpdateAndroidPermissionData("com.picovr.permission.EYE_TRACKING");
@@ -173,8 +187,72 @@ namespace Unity.XR.OpenXR.Features.PICOSupport
                     DeleteAndroidMetaData("picovr.software.eye_tracking");
                     DeleteAndroidMetaData("eyetracking_calibration");
                 }
+                
+                var settings = OpenXRSettings.GetSettingsForBuildTargetGroup(BuildTargetGroup.Android);
+                bool mrPermission = false;
+            
+                foreach (var feature in settings.GetFeatures<OpenXRFeature>())
+                {
 
-                CreateOrUpdateAndroidMetaData("handtracking", PICOProjectSetting.GetProjectConfig().isHandTracking ? "1" : "0");
+                    if (feature is PICOSceneCapture)
+                    {
+                        if (feature.enabled)
+                        {
+                            CreateOrUpdateAndroidMetaData("enable_scene_anchor", "1");
+                         
+                            mrPermission = true;
+                        }
+                        else
+                        {
+                            DeleteAndroidMetaData("enable_scene_anchor");
+                        }
+                    }
+
+                    if (feature is PICOSpatialAnchor)
+                    {
+                        if (feature.enabled)
+                        {
+                            CreateOrUpdateAndroidMetaData("enable_spatial_anchor", "1");
+                            mrPermission = true;
+                        }
+                        else
+                        {
+                            DeleteAndroidMetaData("enable_spatial_anchor");
+                        }
+                    }
+
+                    if (feature is PICOSpatialMesh)
+                    {
+                        if (feature.enabled)
+                        {
+                            CreateOrUpdateAndroidMetaData("enable_mesh_anchor", "1");
+                            mrPermission = true;
+                        }
+                        else
+                        {
+                            DeleteAndroidMetaData("enable_mesh_anchor");
+                        }
+                    }
+
+                }
+                if (PICOProjectSetting.GetProjectConfig().MRSafeguard)
+                {
+                    CreateOrUpdateAndroidMetaData("enable_mr_safeguard", PICOProjectSetting.GetProjectConfig().MRSafeguard ? "1" : "0");
+                }
+                else
+                {
+                    DeleteAndroidMetaData("enable_mr_safeguard");
+                }
+
+                if (mrPermission)
+                {
+                    CreateOrUpdateAndroidPermissionData("com.picovr.permission.SPATIAL_DATA");
+                }
+                else
+                {
+                    DeleteAndroidPermissionData("com.picovr.permission.SPATIAL_DATA");
+                }
+
                 CreateOrUpdateAndroidMetaData("pvr.app.splash", PICOProjectSetting.GetProjectConfig().GetSystemSplashScreen(path));
             }
         }
